b0e2abdf9b1b1e2a5118b859e333315236473c9c
radiospiel <eno@open-lab.org>
2012-06-16 13:19:13 +0200

    Adds request headers section

--- app.rb
+++ app.rb
@@ -6,13 +6,28 @@ if development?
   also_reload 'lib/**/*'
 end
 
+helpers do
+  def format_hash(hash)
+    hash.keys.sort.
+      map do |key, value| [ key, hash[key] ] end.
+      map do |key, value| "%25s: %s\n" % [ key, value.inspect ] end.
+      join
+  end
+end
+
 get(/./) do
   content_type "text/plain"
+
+  lines = []
+  lines << "# Headers\n"
+  lines << format_hash(headers)
+
+  lines << "# Environment\n"
+  lines << format_hash(request.env.select { |k,v| k[0] == k[0].upcase })
+
+  lines << "# Fork me on github\n"
+  lines << "https://github.com/radiospiel/inspector"
   
-  env = request.env
-  
-  env.keys.sort.
-    map do |key, value| [ key, env[key] ] end.
-    map do |key, value| "%25s: %s\n" % [ key, value.inspect ] end
+  lines.join("\n")
 end
 
